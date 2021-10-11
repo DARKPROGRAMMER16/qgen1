f795c8545c206e64cda1dec6575d75aced6b3b2c
Rajat Dash <rajatdash@Rajats-MacBook-Air.local>
2021-10-12 00:56:18 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css'
 
-function App() {
+const App = () => {
+
+  const [quote,setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const quoteAPI = async() => {
+    try {
+      const data = await axios.get("https://api.quotable.io/random");
+      // console.log(data.data);
+      setQuote(data.data.content);
+      setAuthor(data.data.author)
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    quoteAPI();
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Fragment>
+    <section className="quote">
+      <div className="container main-c">
+        <div className="quote-area">
+          <h2>{quote}</h2>
+          <h3 className="text-end mt-4">~{author}</h3>
+          <div className="text-center">
+            <button className="btn btn-outline-success rounded-pill text-center btn-style mt-5 " onClick={quoteAPI}>New Quote</button>
+          </div>
+        </div>
+      </div>
+    </section>
+    </Fragment>
+  )
 }
 
-export default App;
+export default App
